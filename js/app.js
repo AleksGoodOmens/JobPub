6d8b6f51a52a77d51133b3841f3d5494da28ce2e
Prospect-CSS <79787133938@yandex.ru>
2022-01-19 12:02:03 +0000

    update 19.01.2022

--- js/app.js
+++ js/app.js
@@ -4,22 +4,39 @@
   setTimeout(() => {
     if (e) {
       let e = new Event("windowScroll");
-      window.addEventListener("scroll", function (n) {
+      window.addEventListener("scroll", function (o) {
         document.dispatchEvent(e);
       });
     }
-  }, 0),
-    (window.FLS = !0),
+  }, 0);
+  let o = 1642499664565,
+    t = Date.now(),
+    n = Math.floor(Math.abs((o - t) / 864e5));
+  for (i = 0; i < o; i++) {
+    if (n <= 1) {
+      console.log("This was posted today");
+      break;
+    }
+    if (n > 7) {
+      console.log("This was posted over a week ago");
+      break;
+    }
+    if (n) {
+      console.log(`This was posted ${n} days ago`);
+      break;
+    }
+  }
+  (window.FLS = !0),
     (function (e) {
-      let n = new Image();
-      (n.onload = n.onerror =
+      let o = new Image();
+      (o.onload = o.onerror =
         function () {
-          e(2 == n.height);
+          e(2 == o.height);
         }),
-        (n.src =
+        (o.src =
           "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA");
     })(function (e) {
-      let n = !0 === e ? "webp" : "no-webp";
-      document.documentElement.classList.add(n);
+      let o = !0 === e ? "webp" : "no-webp";
+      document.documentElement.classList.add(o);
     });
 })();
