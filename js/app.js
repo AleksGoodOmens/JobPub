3efa3e7b737819c47c42af8494edae4c7ef75319
Prospect-CSS <79787133938@yandex.ru>
2022-01-21 09:23:06 +0000

    update header

--- js/app.js
+++ js/app.js
@@ -1,42 +1,75 @@
 (() => {
   "use strict";
-  let e = !1;
+  let e = !0,
+    t = (t = 500) => {
+      let n = document.querySelector("body");
+      if (e) {
+        let o = document.querySelectorAll("[data-lp]");
+        setTimeout(() => {
+          for (let e = 0; e < o.length; e++) {
+            o[e].style.paddingRight = "0px";
+          }
+          (n.style.paddingRight = "0px"),
+            document.documentElement.classList.remove("lock");
+        }, t),
+          (e = !1),
+          setTimeout(function () {
+            e = !0;
+          }, t);
+      }
+    },
+    n = (t = 500) => {
+      let n = document.querySelector("body");
+      if (e) {
+        let o = document.querySelectorAll("[data-lp]");
+        for (let e = 0; e < o.length; e++) {
+          o[e].style.paddingRight =
+            window.innerWidth -
+            document.querySelector(".wrapper").offsetWidth +
+            "px";
+        }
+        (n.style.paddingRight =
+          window.innerWidth -
+          document.querySelector(".wrapper").offsetWidth +
+          "px"),
+          document.documentElement.classList.add("lock"),
+          (e = !1),
+          setTimeout(function () {
+            e = !0;
+          }, t);
+      }
+    };
+  let o = !1;
   setTimeout(() => {
-    if (e) {
+    if (o) {
       let e = new Event("windowScroll");
-      window.addEventListener("scroll", function (o) {
+      window.addEventListener("scroll", function (t) {
         document.dispatchEvent(e);
       });
     }
-  }, 0);
-  let o = 1642499664565,
-    t = Date.now(),
-    n = Math.floor(Math.abs((o - t) / 864e5));
-  for (i = 0; i < o; i++) {
-    if (n <= 1) {
-      console.log("This was posted today");
-      break;
-    }
-    if (n > 7) {
-      console.log("This was posted over a week ago");
-      break;
-    }
-    if (n) {
-      console.log(`This was posted ${n} days ago`);
-      break;
-    }
-  }
-  (window.FLS = !0),
+  }, 0),
+    (window.FLS = !0),
     (function (e) {
-      let o = new Image();
-      (o.onload = o.onerror =
+      let t = new Image();
+      (t.onload = t.onerror =
         function () {
-          e(2 == o.height);
+          e(2 == t.height);
         }),
-        (o.src =
+        (t.src =
           "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA");
     })(function (e) {
-      let o = !0 === e ? "webp" : "no-webp";
-      document.documentElement.classList.add(o);
-    });
+      let t = !0 === e ? "webp" : "no-webp";
+      document.documentElement.classList.add(t);
+    }),
+    (function () {
+      let o = document.querySelector(".icon-menu");
+      o &&
+        o.addEventListener("click", function (o) {
+          e &&
+            (((e = 500) => {
+              document.documentElement.classList.contains("lock") ? t(e) : n(e);
+            })(),
+            document.documentElement.classList.toggle("menu-open"));
+        });
+    })();
 })();
